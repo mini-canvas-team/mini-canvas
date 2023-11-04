c947fbc4514a5d7d606dea07dcf9b92e6d91f325
hjyan09 <wnwngudwns@gmail.com>
2023-11-04 22:05:01 +0900

    feat: Add ComponentCotainer conections

--- app/src/main/java/canvas/controller/Controller.java
+++ app/src/main/java/canvas/controller/Controller.java
@@ -5,89 +5,110 @@ import canvas.model.Point;
 
 public class Controller {
     private static Controller instance = new Controller();
+    private ComponentContainer componentContainer;
+    private ObjectFactory objectFactory;
     private int selectedObject;
     private Color selectedColor;
 
     private Controller() {
+        componentContainer = new ComponentContainer();
+        objectFactory = new ObjectFactory();
         selectedObject = 0;
         selectedColor = new Color(255, 255, 255);
     }
 
     public static Controller getInstance() {
         return instance;
-    };
+    }
 
     // left panel event
     public void setSelectedObject(int type) {
         selectedObject = type;
-    };
+    }
 
     // down panel event
     public void setSelectedColor(Color color) {
         selectedColor = color;
-    };
+    }
 
     // canvas event
     public void click(Point p) {
-        showProperties();
-    };
+        if (selectedObject == 0) {
+            selectObject(p);
+            showProperties();
+        }
+    }
+
+    public void selectObject(Point p) {
+        componentContainer.selectOne(p);
+    }
 
     public void drag(Point p1, Point p2) {
-        if (selectedObject == 0)
+        if (selectedObject == 0) {
             selectObjects(p1, p2);
-        else
+        } else {
             addObject(p1, p2);
-    };
+            redraw();
+        }
+        showProperties();
+    }
 
     private void selectObjects(Point p1, Point p2) {
+        componentContainer.select(p1, p2);
         showProperties();
-    };
+    }
 
     private void addObject(Point p1, Point p2) {
+        componentContainer.add(objectFactory.create(selectedObject, p1, p2, selectedColor));
         redraw();
-    };
+    }
 
     private void redraw() {
 
-    };
+    }
 
     // right panel event
-    public void changeWidth(int width) {
+    public void changeWidth(Integer width) {
+        componentContainer.setWidth(width);
+        redraw();
+    }
 
-    };
-
-    public void changeHeight(int height) {
-
-    };
+    public void changeHeight(Integer height) {
+        componentContainer.setHeight(height);
+        redraw();
+    }
 
     public void changeColor(Color color) {
-
-    };
+        componentContainer.setColor(color);
+        redraw();
+    }
 
     public void changeIndexFront() {
-
-    };
+        componentContainer.setFront();
+        redraw();
+    }
 
     public void changeIndexBack() {
-
-    };
+        componentContainer.setBack();
+        redraw();
+    }
 
     // right panel function
     private void showProperties() {
         showObjectWidth();
         showObjectHeight();
         showObjectColor();
-    };
+    }
 
     private void showObjectWidth() {
-
-    };
+        Integer width = componentContainer.getWidth();
+    }
 
     private void showObjectHeight() {
-
-    };
+        Integer height = componentContainer.getHeight();
+    }
 
     private void showObjectColor() {
-
-    };
+        Color color = componentContainer.getColor();
+    }
 }
