d48bd814475861ab0e329dddb6d532c6c9df3323
Seungjik Lee <lsjbh45@gmail.com>
2023-11-05 15:33:43 +0900

    fix: Resolve error caused by chooser customizing

--- app/src/main/java/canvas/view/swing/frame/down/ColorChooser.java
+++ app/src/main/java/canvas/view/swing/frame/down/ColorChooser.java
@@ -1,6 +1,7 @@
 package canvas.view.swing.frame.down;
 
 import java.awt.Color;
+import java.util.stream.Stream;
 
 import javax.swing.JColorChooser;
 import javax.swing.JPanel;
@@ -11,15 +12,11 @@ public class ColorChooser extends JColorChooser {
         setPreviewPanel(new JPanel());
         setColor(Color.BLACK);
         AbstractColorChooserPanel[] panels = getChooserPanels();
-        for (int i = 0; i < panels.length; i++) {
-            if (!panels[i].getDisplayName().equals("Swatches")) {
-                removeChooserPanel(panels[i]);
-            } else {
-                JPanel panel = panels[i];
-                System.out.println(panel.getComponentCount()); // 1
-                System.out.println(panel.getPreferredSize()); // width=424,height=112
-                System.out.println(panel.getLayout()); // FlowLayout[hgap=5,vgap=5,align=center]
+
+        Stream.of(panels).forEach(panel -> {
+            if (!panel.getDisplayName().endsWith("(S)")) {
+                removeChooserPanel(panel);
             }
-        }
+        });
     }
 }
