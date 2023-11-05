bcd05616271e5b21d41ed1cf6b637165bac91ca7
hjyan09 <wnwngudwns@gmail.com>
2023-11-05 14:46:05 +0900

    feat: Add Panel for select type

---
+++ app/src/main/java/canvas/view/swing/frame/left/SelectBtn.java
@@ -0,0 +1,17 @@
+package canvas.view.swing.frame.left;
+
+import javax.swing.JButton;
+
+public class SelectBtn extends JButton {
+    private int index;
+
+    public SelectBtn(int i) {
+        super();
+
+        index = i;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
