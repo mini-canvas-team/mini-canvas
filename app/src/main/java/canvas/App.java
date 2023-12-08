709190f616e9a2014fcf2bb0580c075e9f8612ec
Seungjik Lee <lsjbh45@gmail.com>
2023-12-08 18:14:03 +0900

    refactor: Initiate views by its factory

--- app/src/main/java/canvas/App.java
+++ app/src/main/java/canvas/App.java
@@ -1,9 +1,27 @@
 package canvas;
 
-import canvas.view.swing.canvas.CanvasView;
+import java.util.Set;
+
+import canvas.controller.Controller;
+import canvas.controller.Listener;
+import canvas.view.Adapter;
+import canvas.view.View;
+import canvas.view.swing.canvas.CanvasViewFactory;
+import canvas.view.swing.input.InputViewFactory;
 
 public class App {
     public static void main(String[] args) {
-        new CanvasView();
+        Controller controller = Controller.getInstance();
+        Listener listener = controller.getListener();
+
+        Set
+                .of(new CanvasViewFactory(), new InputViewFactory())
+                .forEach(factory -> {
+                    View view = factory.createView(listener);
+                    Set<Adapter> adapters = view.getAdapters();
+                    adapters.forEach(adapter -> {
+                        controller.addAdapter(adapter);
+                    });
+                });
     }
 }
